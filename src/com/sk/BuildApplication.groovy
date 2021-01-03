5158f7b805c9a55667343d37d72f7156f317faca
srinivaskarre <srinivaskarre@rocketmail.com>
2021-01-03 08:43:41 +0530

    second commit

--- src/com/sk/BuildApplication.groovy
+++ src/com/sk/BuildApplication.groovy
@@ -2,8 +2,8 @@ package com.sk
 
 class BuildApplication {
     def build(){
-        echo 'building application'
-        sh 'mvn clean verify'
-        echo 'completed'
+        echo "building application"
+        sh "mvn clean verify"
+        echo "completed"
     }
 }
